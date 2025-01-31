4ce4515aaa9d816acb6dc23d9fa87171232860d3
Nestor Bardel <nestorbardel@gmail.com>
2025-01-31 18:10:48 -0300

    onedark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,43 +1,89 @@
 return {
 	{ "EdenEast/nightfox.nvim" },
 	{
+		"navarasu/onedark.nvim",
+		config = function()
+			-- Lua
+			require("onedark").setup({
+				-- Main options --
+				style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+				transparent = false, -- Show/hide background
+				term_colors = true, -- Change terminal color as per the selected theme style
+				ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+				cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+				-- toggle theme style ---
+				toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+				toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+
+				-- Change code style ---
+				-- Options are italic, bold, underline, none
+				-- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+				code_style = {
+					comments = "italic",
+					keywords = "none",
+					functions = "bold",
+					strings = "none",
+					variables = "bold",
+				},
+
+				-- Lualine options --
+				lualine = {
+					transparent = false, -- lualine center bar transparency
+				},
+
+				-- Custom Highlights --
+				colors = {}, -- Override default colors
+				highlights = {}, -- Override highlight groups
+
+				-- Plugins Config --
+				diagnostics = {
+					darker = true, -- darker colors for diagnostic
+					undercurl = true, -- use undercurl instead of underline for diagnostics
+					background = true, -- use background color for virtual text
+				},
+			})
+		end,
+	},
+	{
 		"eldritch-theme/eldritch.nvim",
 		lazy = false,
 		priority = 1000,
-		config = function() require("eldritch").setup({
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			palette = "darker", -- Available options: "default" (standard palette), "darker" (darker variant)
-			transparent = false, -- Enable this to disable setting the background color
-			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-			styles = {
-				-- Style to be applied to different syntax groups
-				-- Value is any valid attr-list value for `:help nvim_set_hl`
-				comments = { italic = true },
-				keywords = { italic = true },
-				functions = {bold = true},
-				variables = {bold = true},
-				-- Background styles. Can be "dark", "transparent" or "normal"
-				sidebars = "dark", -- style for sidebars, see below
-				floats = "dark", -- style for floating windows
-			},
-			sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-			hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-			dim_inactive = false, -- dims inactive windows, transparent must be false for this to work
-			lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+		config = function()
+			require("eldritch").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				palette = "darker", -- Available options: "default" (standard palette), "darker" (darker variant)
+				transparent = false, -- Enable this to disable setting the background color
+				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+				styles = {
+					-- Style to be applied to different syntax groups
+					-- Value is any valid attr-list value for `:help nvim_set_hl`
+					comments = { italic = true },
+					keywords = { italic = true },
+					functions = { bold = true },
+					variables = { bold = true },
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "dark", -- style for sidebars, see below
+					floats = "dark", -- style for floating windows
+				},
+				sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+				hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+				dim_inactive = false, -- dims inactive windows, transparent must be false for this to work
+				lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
 
-			--- You can override specific color groups to use other groups or a hex color
-			--- function will be called with a ColorScheme table
-			---@param colors ColorScheme
-			on_colors = function(colors) end,
+				--- You can override specific color groups to use other groups or a hex color
+				--- function will be called with a ColorScheme table
+				---@param colors ColorScheme
+				on_colors = function(colors) end,
 
-			--- You can override specific highlights to use other groups or a hex color
-			--- function will be called with a Highlights and ColorScheme table
-			---@param highlights Highlights
-			---@param colors ColorScheme
-			on_highlights = function(highlights, colors) end,
-		})
-        end,
+				--- You can override specific highlights to use other groups or a hex color
+				--- function will be called with a Highlights and ColorScheme table
+				---@param highlights Highlights
+				---@param colors ColorScheme
+				on_highlights = function(highlights, colors) end,
+			})
+		end,
 	},
 	{
 		"LazyVim/LazyVim",
